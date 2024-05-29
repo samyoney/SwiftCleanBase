61eb206bafc1a23bf7bfe5e88458aa0705c5ee34
サム <>
2024-05-29 15:12:47 +0900

    Init app

---
+++ SwiftCleanBase/SwiftCleanBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCleanBaseApp.swift
+//  SwiftCleanBase
+//
+//  Created by グェン・ホン・ソン on 2024/05/29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCleanBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
